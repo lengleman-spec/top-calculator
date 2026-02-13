3235bc487b6a1af3922bc572181b70ecbc3b338b
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-02-13 15:03:18 -0500

    Add logic to add numbers and decimals together.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// function add(num1, num2) {
-//   return num1 + num2;
-// }
 // Grabbing the display element:
 const display = document.getElementById("display");
 
+// Calculator state:
+let currentInput = "";
+let previousInput = "";
+let operator = "";
+
+// Add a number or decimal
 function appendNumber(num) {
+  if (num === "." && currentInput.includes(".")) return;
   currentInput += num;
   display.textContent = currentInput;
 }
